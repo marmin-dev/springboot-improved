984c4d36ef97d87cbf107acefba0dc7212a6db29
Marmin <marmin.dev@gmail.com>
2022-11-27 16:10:06 +0900

    add hello Dto and lombok and test hello controller

---
+++ src/test/java/com/improved/springboot/web/HelloControllerTest.java
@@ -0,0 +1,29 @@
+package com.improved.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers=HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired//스프링이 관리하는 빈을 주입받는다
+    private MockMvc mvc;
+
+    @Test
+    public void hello_return() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
