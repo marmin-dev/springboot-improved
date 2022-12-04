54af2338f2e95296209cf9de9de1f50f9bbc9d40
Marmin <marmin.dev@gmail.com>
2022-12-04 15:10:20 +0900

    debug 500 error and update func

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -4,9 +4,11 @@ var main ={
         $('#btn-save').on('click',function(){
         _this.save();
         });
+
         $('#btn-update').on('click',function(){
-        _this.update
+        _this.update();
         });
+
     },
     save : function(){
         var data={
@@ -32,16 +34,18 @@ var main ={
         title: $('#title').val(),
         content: $('#content').val()
         };
+
         var id = $('#id').val();
+
         $.ajax({
         type: 'PUT',
-        url: '/api/v1/posts' + id,
+        url: '/api/v1/posts/' + id,
         dataType: 'json',
         contentType: 'application/json; charset=utf-8',
         data: JSON.stringify(data)
         }).done(function(){
         alert('글이 수정되었습니다');
-        window.location.href('/');
+        window.location.href= '/';
         }).fail(function(error){
         alert(JSON.stringify(error));
         });
